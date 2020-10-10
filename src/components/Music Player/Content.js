8a58dae794dd0b58b91cb05da0ce88409526fea4
bazelajoanna <bazela.joanna@gmail.com>
2020-10-10 14:56:02 +0200

    Add React Context

--- src/components/Music Player/Content.js
+++ src/components/Music Player/Content.js
@@ -1,12 +1,53 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import {StoreContext} from "./index";
 
 
-const Content = ({children}) => {
+const Content = () => {
+    const {state, dispatch} = useContext(StoreContext);
+    const currentPlaylist = state.currentPlaylist;
+    const songIds = Array.from(state.playlists[currentPlaylist])
     return (
         <div className="Content">
-            {children}
+            <div className="playlist-title">{currentPlaylist}</div>
+
+            {songIds.length <= 0 ? (
+                <p>You need to add your favourite songs!</p>
+            ) : (
+                <table>
+
+                    <tr>
+                        <td></td>
+                        <td>Title</td>
+                        <td>Artist</td>
+                        <td>Length</td>
+                    </tr>
+
+                    <tbody>
+                    {songIds.map(id => {
+                        const {title, artist, length} = state.media[id];
+                        const isFavourite = state.playlists.favourites.has(id)
+
+                        return (
+                            <tr key={id}>
+                                <td>
+                                    {
+                                        isFavourite ? (<i className="fas fa-heart"
+                                                          onClick={() => dispatch({ type: "REMOVE_FAVOURITE", songId: id})}/>) :
+                                            (<i className="far fa-heart"
+                                            onClick={() => dispatch({type: "ADD_FAVOURITE", songId: id})}/>)
+                                    }
+                                </td>
+                                <td>{title}</td>
+                                <td>{artist}</td>
+                                <td>{length}</td>
+                            </tr>
+                        )
+                    })}
+                    </tbody>
+                </table>
+            )}
         </div>
     );
-}
+};
 
 export default Content;
--- src/components/Music Player/index.js
+++ src/components/Music Player/index.js
@@ -1,19 +1,53 @@
-import React from 'react';
+import React, {useReducer, createContext} from 'react';
 import Topbar from "./Topbar";
 import Sidebar from "./Sidebar";
 import Playbar from "./Playbar";
 import Content from "./Content";
+import media from "../../media";
 
+export const StoreContext = createContext(null);
+
+const initialState = {
+    currentPlaylist: "home",
+    media,
+    playlists: {
+        home: new Set(media.ids),
+        favourites: new Set()
+    },
+};
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case "ADD_PLAYLIST":
+            return {
+                ...state,
+                playlists: {...state.playlists, [action.playlist]: new Set()}
+            };
+        case "SET_PLAYLIST":
+            return {...state, currentPlaylist: action.playlist}
+        case "ADD_FAVOURITE":
+            state.playlists.favourites.add(action.songId)
+            return {...state}
+        case "REMOVE_FAVOURITE":
+            state.playlists.favourites.delete(action.songId)
+            return {...state}
+
+    }
+    return state
+};
 
 
 const MusicPlayer = () => {
+    const [state, dispatch] = useReducer(reducer, initialState);
     return (
-        <div className="MusicPlayer">
-            <Topbar/>
-            <Sidebar/>
-            <Content/>
-            <Playbar/>
-        </div>
+        <StoreContext.Provider value={{state, dispatch}}>
+            <div className="MusicPlayer">
+                <Topbar/>
+                <Sidebar/>
+                <Content/>
+                <Playbar/>
+            </div>
+        </StoreContext.Provider>
     );
 };
 
